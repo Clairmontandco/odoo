e1f1f2a837abc5840e77fda4890dbddab6ae095d
Vishal Kapuriya <vishal@keypress.co.in>
2023-10-11 15:30:58 +0530

    add changes related to delivery forecast

--- kits_mrp_customization/models/sale_order_line.py
+++ kits_mrp_customization/models/sale_order_line.py
@@ -4,3 +4,5 @@ class sale_order_line(models.Model):
     _inherit='sale.order.line'
 
     delivery_date = fields.Date('Delivery Date',related='order_id.validity_date',store=True)
+    product_category_id = fields.Many2one('product.category','Product Category',related='product_id.categ_id',store=True)
+    product_tags_ids = fields.Many2many('x_product_tags','orderline_producttags_rel','order_line_id','x_product_tag_id','Product Tag',related='product_id.x_studio_many2many_field_bOjgj')
