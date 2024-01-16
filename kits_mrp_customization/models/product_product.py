9d27ee1cefb3393ea8036f145f9ce655d4761e29
Vishal Kapuriya <vishal@keypress.co.in>
2024-01-16 19:09:51 +0530

    add bom and manufecturing reference

--- kits_mrp_customization/models/product_product.py
+++ kits_mrp_customization/models/product_product.py
@@ -10,6 +10,8 @@ from odoo.exceptions import UserError
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
+    manufacturing_default_code = fields.Char('Manufacturing Internal Reference')
+
     # Method for monthly data of current and previws year.
     def get_report_data(self):
         product_report_data = {}
@@ -1109,12 +1111,34 @@ class ProductProduct(models.Model):
             'target': 'self',
         }
 
-
-
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     is_kcash_rewards = fields.Boolean('K-Cash Product')
     hide_from_order = fields.Boolean('Hide From Order')
 
-    
+    manufacturing_default_code = fields.Char('Manufacturing Internal Reference',compute='_compute_manufacturing_default_code',inverse='_set_manufacturing_default_code',store=True)
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        templates = super(ProductTemplate, self).create(vals_list)
+        for template, vals in zip(templates, vals_list):
+            related_vals = {}
+            if vals.get('manufacturing_default_code'):
+                related_vals['manufacturing_default_code'] = vals['manufacturing_default_code']
+            if related_vals:
+                template.write(related_vals)
+        return templates
+
+    @api.depends('product_variant_ids', 'product_variant_ids.manufacturing_default_code')
+    def _compute_manufacturing_default_code(self):
+        unique_variants = self.filtered(lambda template: len(template.product_variant_ids) == 1)
+        for template in unique_variants:
+            template.manufacturing_default_code = template.product_variant_ids.manufacturing_default_code
+        for template in (self - unique_variants):
+            template.manufacturing_default_code = False
+
+    def _set_manufacturing_default_code(self):
+        for template in self:
+            if len(template.product_variant_ids) == 1:
+                template.product_variant_ids.manufacturing_default_code = template.manufacturing_default_code
