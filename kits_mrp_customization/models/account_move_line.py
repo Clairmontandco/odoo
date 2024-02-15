5acfb5c1f127b12361e1befe307551d80c4f9568
Vishal Kapuriya <vishal@keypress.co.in>
2024-02-15 18:38:46 +0530

    revert changes

--- kits_mrp_customization/models/account_move_line.py
+++ kits_mrp_customization/models/account_move_line.py
@@ -27,16 +27,6 @@ class AccountMove(models.Model):
     _inherit='account.move'
 
     delivery_status = fields.Selection([('draft', 'Draft'),('waiting', 'Waiting Another Operation'),('confirmed', 'Waiting'),('assigned', 'Ready'),('done', 'Done'),('cancel', 'Cancelled')],compute='_compute_delivery_status',default=None)
-    
-    paid_date = fields.Date('Paid Date',compute='_compute_paid_date',store=True)
-
-    date_done = fields.Date('Ship Date',compute='_compute_delivery_status',store=True)
-
-    def _compute_paid_date(self):
-        for rec in self:
-            payments = rec._get_reconciled_payments()
-            latest_paid_date = max(payments, key=lambda x: x.create_date).create_date.date() if payments else None
-            rec.paid_date = latest_paid_date
 
     def _compute_delivery_status(self):
         for rec in self:
@@ -45,4 +35,3 @@ class AccountMove(models.Model):
                 sale_order = self.env['sale.order'].search([('name','=',rec.invoice_origin)])
                 picking_ids = sale_order.picking_ids.filtered(lambda x:x.state != 'cancel') if sale_order else None
                 rec.delivery_status = max(picking_ids, key=lambda x: x.create_date).state if picking_ids else None
-                rec.date_done = max(picking_ids, key=lambda x: x.create_date).date_done if picking_ids else None
