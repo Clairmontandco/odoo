5a0126c18ac25a08e23c02115732db0880f73d95
Vishal Kapuriya <vishal@keypress.co.in>
2024-03-01 17:58:34 +0530

    add kcash related changes

--- kits_mrp_customization/models/kcash_bonus.py
+++ kits_mrp_customization/models/kcash_bonus.py
@@ -28,11 +28,19 @@ class kcash_bonus(models.Model):
         rec = super(kcash_bonus, self).default_get(fields)
         return rec
 
-    @api.depends('sale_id')
+    @api.depends('sale_id','kcash_type')
     def _compute_domain_sale_order(self):
         for rec in self:
             order_ids = rec.partner_id.kcash_bonus_ids.mapped('sale_id').ids
-            rec.write({
-                'domain_order_ids' : [(6,0,order_ids)]
-            })
-            
+            if rec.kcash_type == 'reward':
+                rec.write({
+                    'domain_order_ids' : [(6,0,order_ids)]
+                })
+            else:
+                rec.domain_order_ids = None
+
+    @api.onchange('kcash_type')
+    def onchange_kcash_type(self):
+        for rec in self:
+            if (rec.kcash_type == 'reward') and (rec.sale_id.id in rec.domain_order_ids.ids):
+                rec.sale_id = None
